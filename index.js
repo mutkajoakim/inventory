8245a7df940d446a8f908f4f872d37fa349ba90a
mutkajoakim <43183128+mutkajoakim@users.noreply.github.com>
2021-02-28 18:28:28 +0200

    added cors

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config')
 
+app.use(cors());
 app.use(bodyParser.json());
 let port = process.env.PORT || 3000;
 
